8e2b4e374d0482d1588a23f9c54b94c3989537ba
HEEEAM <130165427+HEEEAM@users.noreply.github.com>
2026-03-08 14:44:10 +0900

    Update record.tsx (일정 -> 레이아웃 구조 + BD 연결 로직)

--- frontend/app/(tabs)/record.tsx
+++ frontend/app/(tabs)/record.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
-export default function Record() {
+export default function TabLayout() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>📔 기록 화면</Text>
-    </View>
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "홈",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="record"
+        options={{
+          title: "기록",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="mic" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="schedule"
+        options={{
+          title: "일정",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
